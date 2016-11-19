e718137cb6acae753841d666e3ee93a9197cb7e8
bachtsui <dbtsui2@gmail.com>
2016-11-19 14:59:45 -0800

    Added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,20 +7,21 @@ yongle = Emperor.create(name: 'YongLe', year: '1402-1424', dynasty:'Ming')
 wanli = Emperor.create(name: 'WanLi', year:'1572-1582', dynasty:'Ming')
 tianqi = Emperor.create(name: 'TianQi', year:'1620-1627', dynasty: 'Ming')
 
-
-two = Emperor.create(name:'Two', year: '2016', dynasty: 'Today')
-
 # Seed Data for Traits
 wld = Trait.create(name:'Wo Lun Di (Thumb Fits Snugly)')
-yzy = Trait.create(name: 'Ya Zha Ye (Duckweb Leaves)')
 rt = Trait.create(name: 'Ru Tuo (Bump in the Middle of Base)')
+zy = Trait.create(name: 'Zhe Yao (Bend in Plate)')
 sy = Trait.create(name: 'Shua You (Painted Bottom)')
+yzy = Trait.create(name: 'Ya Zha Ye (Duckweb Leaves)')
 
 # Pushing Traits to Emperors
 hongwu.traits << wld
-wanli.traits << yzy
 hongwu.traits << rt
+hongwu.traits << zy
 yongle.traits << sy
+wanli.traits << yzy
+
+
 
 
 # tianqi.traits << rt Comment: One to One Relationship, didn't set up multiple yet
