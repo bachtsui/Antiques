d16cd24559c12be4f119c8af967d0b9e4361ec4d
bachtsui <dbtsui2@gmail.com>
2016-12-18 16:48:47 -0800

    Added an Emperor's Show page

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   root to: "welcome#index"
 
   #Routes for Traits
-  get "/traits", to: "traits#index"
+  get "/traits", to: "traits#index", as:"traits"
 
   #Routes for Emperor
+  get "/emperors", to:"emperors#index", as:"emperors"
   get "/emperors/:id", to:"emperors#show", as:"emperor"
 end
