404fa7070553a68714559f3954a7a42c2f8bce0e
bachtsui <dbtsui2@gmail.com>
2016-12-18 16:59:58 -0800

    Made an emperor's show page, embellished seed data

--- app/controllers/emperors_controller.rb
+++ app/controllers/emperors_controller.rb
@@ -1,5 +1,10 @@
 class EmperorsController < ApplicationController
 
+	def index
+		@emperors = Emperor.all
+		render :index
+	end
+
 	def show
 		@emperor = Emperor.find_by_id(params[:id])
 	end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,9 +3,9 @@ Trait.destroy_all
 
 # Seed Data for Emperors
 hongwu = Emperor.create(name:'HongWu (洪武)', year: '1368-1399', dynasty: 'Ming')
-yongle = Emperor.create(name: 'YongLe', year: '1402-1424', dynasty:'Ming')
-wanli = Emperor.create(name: 'WanLi', year:'1572-1582', dynasty:'Ming')
-tianqi = Emperor.create(name: 'TianQi', year:'1620-1627', dynasty: 'Ming')
+yongle = Emperor.create(name: 'YongLe(永樂)', year: '1402-1424', dynasty:'Ming')
+wanli = Emperor.create(name: 'WanLi(萬曆)', year:'1572-1582', dynasty:'Ming')
+tianqi = Emperor.create(name: 'TianQi(天啓)', year:'1620-1627', dynasty: 'Ming')
 
 # Seed Data for Traits
 wld = Trait.create(name:'Wo Lun Di (Thumb Fits Snugly)')
